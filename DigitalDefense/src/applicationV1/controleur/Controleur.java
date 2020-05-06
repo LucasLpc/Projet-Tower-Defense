1e43c6b88811e6c7276086dae02331c835ef9b4a
ARNDCLZD <64208880+ARNDCLZD@users.noreply.github.com>
2020-05-06 17:35:33 +0200

    Déplacement de personnages sur console

--- DigitalDefense/src/applicationV1/controleur/Controleur.java
+++ DigitalDefense/src/applicationV1/controleur/Controleur.java
@@ -1,5 +1,83 @@
 package applicationV1.controleur;
 
-public class Controleur {
+import java.net.URL;
+import java.util.ResourceBundle;
 
-}
+import applicationV1.modele.*;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.ListChangeListener;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+
+public class Controleur implements Initializable {
+	private Environnement envTest;
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {	
+		this.envTest = new Environnement(300,300);
+		
+	}
+
+	@FXML
+	void ajouter(ActionEvent event) {
+		if(nbEntité.getText().isEmpty()) {
+			throw new Error("Veuillez entrer un nombre d'Animaux.");
+		}
+		int nbAnimaux = Integer.parseInt(nbEntité.getText());
+		if(toggleMouton.isSelected())
+			for(int i=0; i<nbAnimaux;i++) {
+				Mouton m = new Mouton(envTest);
+				this.envTest.ajouter(m);
+				creerSprite(m);
+				System.out.println("Ajout d'un mouton.");
+			}
+		else if(toggleLoup.isSelected())
+			for(int i=0; i<nbAnimaux;i++) {
+				Loup l = new Loup(envTest);
+				this.envTest.ajouter(l);
+				creerSprite(l);
+				System.out.println("Ajout d'un loup.");
+			}
+		else throw new Error("Veuillez selectionner un type d'acteur.");
+	}
+
+	@FXML
+	void unTour(ActionEvent event) {
+		this.envTest.unTour();
+		System.out.println(Mouton.getReproduction()+","+Loup.getReproduction());
+//		refresh();
+	}
+
+	@FXML
+	void faireDesTours(ActionEvent event) {
+		int nbtours = Integer.parseInt(this.nbTours.getText());
+		for (int i=0; i<nbtours; i++) {
+			this.envTest.unTour();
+//			refresh();
+		}
+	}
+	void creerSprite(Ennemi a) {
+		Circle r;
+		if(a instanceof Mouton) {
+			r = new Circle(2);
+			r.setFill(Color.WHITE);
+		}
+		else {
+			r = new Circle(3);
+			r.setFill(Color.RED);
+		}
+		r.setOnMouseClicked((e) -> System.out.println(a));
+		r.translateXProperty().bind(a.getXProperty());
+		r.translateYProperty().bind(a.getYProperty());
+		midPane.getChildren().add(r);
+	}
---
+++ DigitalDefense/src/applicationV1/modele/Essai.java
@@ -0,0 +1,22 @@
+package applicationV1.modele;
+
+public class Essai {
+
+	public static void main(String[] args) {
+		Environnement env = new Environnement(5,5);
+		env.initTerrain();
+		env.afficherTerrain();
+		Ennemi e = new EnnemiTypeBasic(2,2,env);
+		Ennemi e2 = new EnnemiTypeBasic(3,2,env);
+		env.ajouter(e);
+		env.ajouter(e2);
+		
+		for(int i=0;i<5;i++) {
+			env.refresh();
+		}
+		
+		
+		
+	}
+
+}
